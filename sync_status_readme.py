895a7c58538df8da6fc0433f34c4167c5d54b28f
Muxin <100744989+muxin-web3@users.noreply.github.com>
2025-04-13 14:20:42 +1200

    Update sync_status_readme.py

--- sync_status_readme.py
+++ sync_status_readme.py
@@ -10,7 +10,7 @@ import logging
 START_DATE = datetime.fromisoformat(os.environ.get(
     'START_DATE', '2025-03-31T00:00:00+00:00')).replace(tzinfo=pytz.UTC)
 END_DATE = datetime.fromisoformat(os.environ.get(
-    'END_DATE', '2025-04-12T23:59:59+00:00')).replace(tzinfo=pytz.UTC)
+    'END_DATE', '2025-04-12T23:59:59+08:00')).replace(tzinfo=pytz.UTC)
 DEFAULT_TIMEZONE = 'Asia/Shanghai'
 FILE_SUFFIX = '.md'
 README_FILE = 'README.md'
